7d56b6d13482c4960ac319245e260b639da1c244
jawsbar <radio700@hanmail.net>
2023-06-21 19:41:53 +0900

    rowQuery

--- src/main/java/jaws/com/Repository/CarRepo.java
+++ src/main/java/jaws/com/Repository/CarRepo.java
@@ -8,25 +8,20 @@ import java.util.List;
 
 
 public interface CarRepo extends CrudRepository<Car,Long>{
-    // List<Car> findByBrand(String brand);
     List<Car> findByColor(String Color);
     List<Car> findByYear(int year);
 
     List<Car> findByBrandAndModel(String Brand, String Model);
     List<Car> findByBrandAndColor(String Brand, String Color);
 
-    // //정렬
+    //정렬
     List<Car> findByBrandOrderByYearAsc(String Brand);
     
     //row query
-    //@Query("select c from Car c where c.brand = ?1")
-    //@Query("SELECT C.* FROM Car C WHERE C.brand = ?1")
     @Query(value = "SELECT C FROM Car C WHERE C.brand = ?1")
-    List<Car> findByBrand(String brand);
-    // @Query(value = "SELECT * FROM CAR WHERE BRAND = ?1")
-    // List<Car> findByBrand(String brand);
+    List<Car> findByBrand(String Brand);
 
-    // @Query(value = "SELECT * FROM CAR WHERE BRAND LIKE %?%1")
-    // List<Car> findByBrandEndsWith(String brand);
+    @Query(value = "SELECT C FROM Car C WHERE C.color = ?1")
+    List<Car> findByColorWithQuery(String Color);
 
 }
